ce877107aa8750d48ceccc357a56463740807e66
mattsouza26 <74125222+mattsouza26@users.noreply.github.com>
2020-11-08 00:46:43 -0300

    Update kali.sh

--- Scripts/Installer/Kali/kali.sh
+++ Scripts/Installer/Kali/kali.sh
@@ -24,7 +24,7 @@ if [ "$first" != 1 ];then
 		*)
 			echo "unknown architecture"; exit 1 ;;
 		esac
-		wget "https://raw.githubusercontent.com/EXALAB/AnLinux-Resources/master/Rootfs/Kali/${archurl}/kali-rootfs-${archurl}.tar.xz" -O $tarball
+		wget "https://github.com/EXALAB/Anlinux-Resources/tree/master/Rootfs/Kali/${archurl}/kali-rootfs-${archurl}.tar.xz" -O $tarball
 	fi
 	cur=`pwd`
 	mkdir -p "$folder"
